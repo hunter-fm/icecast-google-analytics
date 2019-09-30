983c33a5bdbba9429a46569e7cce1010d749c0a2
Gustavo Minari <gustavo_minari@hotmail.com.br>
2019-09-30 03:39:35 -0300

    fix: 🐛 Event and Session

--- src/lib/analytics.js
+++ src/lib/analytics.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
 export default class Analytics {
-  constructor(config, urlbase, minConnect) {    
-    this.sessionType = this.sessionType.bind(this);
+  constructor(config, urlbase, minConnect) {
+    this.data2Uri = this.data2Uri.bind(this);
+
+    this.getSession = this.getSession.bind(this);
+    this.createSession = this.createSession.bind(this);
+
     this.createUserEvent = this.createUserEvent.bind(this);
     this.createUserPageview = this.createUserPageview.bind(this);
     this.updateMounts = this.updateMounts.bind(this);
@@ -16,22 +20,35 @@ export default class Analytics {
     this.urlgoogle = 'https://www.google-analytics.com/batch';
   }
 
-  sessionType(uuid, info) {
-    const session = this.sessions.find(c => c.uuid === uuid);
-    if (session === undefined || session === null) {
-      this.sessions.push({ info, uuid, update: this.update });
-      return 'start';
-    }
-
-    session.update = this.update;
-    session.info = Object.assign({}, info);
-    return 'update';
+  data2Uri(data) {
+    let textData = '';
+    Object.keys(data).forEach(key => {
+      if (textData.length > 0) textData += '&';
+      textData += `${key}=${encodeURIComponent(data[key])}`;
+    });
+    return textData;
   }
-  
+
+  getSession(uuid) {
+    const s = this.sessions.find(c => c.uuid === uuid);
+    if (s !== undefined && s !== null) return s;
+    return null;
+  }
+
+  createSession(uuid, info) {
+    const s = Object.assign({}, { info, uuid, update: this.update });
+    this.sessions.push(s);
+  }
+
   createUserEvent(mount, listener) {
-    let title = mount.metadata.title || '';
-    if ('artist' in mount.metadata) {
-      title = `${mount.metadata.artist || ''} - ${title}`;
+    let title = '';
+    if (mount.metadata !== null) {
+      title = mount.metadata.title || '';
+      if ('artist' in mount.metadata) {
+        title = `${mount.metadata.artist || ''} - ${title}`;
+      }
+    } else {
+      title = mount.title || '';
     }
 
     const isAds = title.toLowerCase().indexOf('advert:') === 0;
@@ -44,66 +61,45 @@ export default class Analytics {
       ea: isAds ? 'advert' : 'music',
       el: title,
 
-      cid: listener.ip,
+      cid: listener.uuid,
       uip: listener.ip,
       ua: listener.useragent,
       dh: this.urlbase,
-      dp: `${mount.name}/${title}`,
-      dt: mount.title,
+      dp: 'url' in this.config ? this.config.url : mount.name,
       dr: listener.referer,
     };
 
-    let textData = '';
-    Object.keys(data).forEach(key => {
-      if (textData.length > 0) textData += '&';
-      textData += `${key}=${encodeURIComponent(data[key])}`;
-    });
-    return textData;
+    return this.data2Uri(data);
   }
 
   createUserPageview(mount, listener) {
-    let title = mount.metadata.title || '';
-    if ('artist' in mount.metadata) {
-      title = `${mount.metadata.artist || ''} - ${title}`;
-    }
-
+    // let title = mount.metadata.title || '';
+    // if ('artist' in mount.metadata) {
+    //   title = `${mount.metadata.artist || ''} - ${title}`;
+    // }
+    // dt: mount.title,
     const data = {
       v: 1,
       t: 'pageview',
-      sc: 'update',
+      sc: 'start',
       tid: this.config.TID,
       cid: listener.uuid,
       uip: listener.ip,
       ua: listener.useragent,
       dh: this.urlbase,
-      dp: mount.name,
-      dt: mount.title,
+      dp: 'url' in this.config ? this.config.url : mount.name,
       dr: listener.referer,
     };
 
-    data.sc = this.sessionType(listener.uuid, data);
-
-    let textData = '';
-    Object.keys(data).forEach(key => {
-      if (textData.length > 0) textData += '&';
-      textData += `${key}=${encodeURIComponent(data[key])}`;
-    });
-    return textData;
+    this.createSession(listener.uuid, data);
+    return this.data2Uri(data);
   }
 
   createUserSessionEnd() {
     const ends = this.sessions.filter(s => s.update !== this.update);
     this.sessions = this.sessions.filter(s => s.update === this.update);
     return ends.length > 0
-      ? ends.map(s => {
-          const data = { ...s.info, sc: 'end' };
-          let textData = '';
-          Object.keys(data).forEach(key => {
-            if (textData.length > 0) textData += '&';
-            textData += `${key}=${encodeURIComponent(data[key])}`;
-          });
-          return textData;
-        })
+      ? ends.map(s => this.data2Uri({ ...s.info, sc: 'end' }))
       : [];
   }
 
@@ -119,17 +115,19 @@ export default class Analytics {
     // Gerar os id's e envia para o Google Analytics
     this.update++;
     let datas = [];
+
     myMounts.forEach(m => {
-      const valores = m.listeners
-        .filter(l => l.connect >= this.minConnect)
-        .map(l =>
-          this.config.type.map(type => {
-            return type === 'pageview'
-              ? this.createUserPageview(m, l)
-              : this.createUserEvent(m, l);
-          })
-        );
-      datas = [...datas, ...valores];
+      m.listeners.forEach(l => {
+        if (l.connect < this.minConnect) return;
+
+        const s = this.getSession(l.uuid);
+        if (s === null) {
+          datas.push(this.createUserPageview(m, l));
+        } else {
+          s.update = this.update;
+        }
+        datas.push(this.createUserEvent(m, l));
+      });
     });
     datas = [...datas, ...this.createUserSessionEnd()];
 
